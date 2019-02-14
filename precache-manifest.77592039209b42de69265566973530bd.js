0366894f445b36eccda366ed6e2e5e098e5cd324
Sam O'Brien <samcob@uw.edu>
2019-02-13 17:25:28 -0800

    Updates

---
+++ precache-manifest.77592039209b42de69265566973530bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "798d395f143f646e57ad",
+    "url": "/Samcob/firstreactapp.git/static/css/main.6bd7cc6f.chunk.css"
+  },
+  {
+    "revision": "798d395f143f646e57ad",
+    "url": "/Samcob/firstreactapp.git/static/js/main.798d395f.chunk.js"
+  },
+  {
+    "revision": "82c5f5429c08be414266",
+    "url": "/Samcob/firstreactapp.git/static/js/runtime~main.82c5f542.js"
+  },
+  {
+    "revision": "32a8655bdd220858fff2",
+    "url": "/Samcob/firstreactapp.git/static/js/2.32a8655b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Samcob/firstreactapp.git/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "27e6aae4ef3b8f51051ce41b0f2d81e8",
+    "url": "/Samcob/firstreactapp.git/index.html"
+  }
+];
